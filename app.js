db9dbe790a45a75e1bca3c15a4cb1d9f55cf3e5b
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-12 22:46:55 +0000

    added a simple list of dummy posts

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ angular.module("flapperNews", [])
 .controller('MainCtrl', [ '$scope',
   function($scope) {
     $scope.test = "Hello World";
+    $scope.posts = [ 'post 1', 'post 2', 'post 3', 'post 4', 'post 5']
 }]);
