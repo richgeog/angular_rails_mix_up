a9c7c87bc5257f561dfba8293dc8b85ec62f27bc
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-12 23:40:36 +0000

    user input for title is now showing

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ angular.module("flapperNews", [])
       {title: 'post 5', upvotes: 4}
     ];
     $scope.addPost = function() {
-      $scope.posts.push({title: 'A new post', upvotes: 0});
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
     };
 }]);
