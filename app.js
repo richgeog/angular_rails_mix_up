b9d48f478489db30e51581cc162606a6b00fac08
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-13 15:08:39 +0000

    up voting functionality possible. Removed the dummy data

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ angular.module("flapperNews", [])
 .controller('MainCtrl', [ '$scope',
   function($scope) {
     $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
     ];
+
     $scope.addPost = function() {
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
-      $scope.title = '';
+      if(!$scope.title || $scope.title === '')
+        { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        upvotes: 0
+      });
+
+    $scope.title = '';
+
+    $scope.incrementUpVotes = function(post) {
+      post.upvotes += 1;
     };
+  };
 }]);
