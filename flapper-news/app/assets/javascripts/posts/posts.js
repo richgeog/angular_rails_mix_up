5befe23c6dca676e79169acbe872042dfc8981f4
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-15 18:17:37 +0000

    loading posts in the database

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -1,8 +1,17 @@
 angular.module("flapperNews")
 
-.factory('posts', [function() {
-  var o = {
-    posts: []
+.factory('posts', ['$http',
+  function($http) {
+    var o = {
+      posts: []
+    };
+
+  o.getAll = function() {
+    return $http.get('/posts.json').success(function(data) {
+      angular.copy(data, o.posts);
+    });
   };
+
   return o;
 }]);
+
